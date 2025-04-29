d643d03c1b86076f7268685a38b21f34f4e33c6b
tecosaur <tecosaur@users.noreply.github.com>
2025-04-29 15:37:05 +0000

    Add  TTFX snippet: Cache

---
+++ B/BaseDirs/Cache/task.jl
@@ -0,0 +1,20 @@
+# Task: Cache
+# Package: BaseDirs
+# Author: @tecosaur
+# Created: 2025-04-29T15:37:02.809
+
+__t1 = time()
+
+using BaseDirs
+
+__t2 = time()
+
+BaseDirs.User.cache()
+
+__t3 = time()
+
+__t_using = __t2 - __t1
+__t_script = __t3 - __t2
+__t_total = __t3 - __t1
+println(stdout, "($__t_using, $__t_script, $__t_total) seconds")
+

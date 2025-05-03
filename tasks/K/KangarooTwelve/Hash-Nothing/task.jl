19a9b2407ee37f78dbb4976f7a8831c3f1839895
tecosaur <tecosaur@users.noreply.github.com>
2025-05-03 09:01:25 +0000

    Add  TTFX snippet: Hash nothing

---
+++ tasks/K/KangarooTwelve/Hash-Nothing/task.jl
@@ -0,0 +1,21 @@
+# Task: Hash Nothing
+# Package: KangarooTwelve
+# Author: @tecosaur
+# Created: 2025-05-03
+# Sample timings: install in 23.722s, run in 0.121s
+
+__t1 = time()
+
+using KangarooTwelve
+
+__t2 = time()
+
+k12(UInt8[])
+
+__t3 = time()
+
+__t_using = __t2 - __t1
+__t_script = __t3 - __t2
+__t_total = __t3 - __t1
+println(stdout, "$__t_using, $__t_script, $__t_total seconds")
+

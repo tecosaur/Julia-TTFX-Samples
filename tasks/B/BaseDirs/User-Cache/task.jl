1d7f63775bd423051f3d2dc31f68be1d284667d8
tecosaur <tecosaur@users.noreply.github.com>
2025-05-03 09:06:58 +0000

    New task: BaseDirs, User cache

---
+++ tasks/B/BaseDirs/User-Cache/task.jl
@@ -0,0 +1,21 @@
+# Task: User Cache
+# Package: BaseDirs
+# Author: @tecosaur
+# Created: 2025-05-03
+# Sample timings: install in 5.9s, run in 0.02s
+
+__t1 = time()
+
+using BaseDirs
+
+__t2 = time()
+
+BaseDirs.User.cache()
+
+__t3 = time()
+
+__t_using = __t2 - __t1
+__t_script = __t3 - __t2
+__t_total = __t3 - __t1
+println(stdout, "$__t_using, $__t_script, $__t_total seconds")
+

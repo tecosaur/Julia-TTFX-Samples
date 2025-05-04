20e391764f1e1ef1936734f60d0339b2c1322a42
joa-quim <joa-quim@users.noreply.github.com>
2025-05-04 23:22:36 +0000

    New task: GMT, Create a xy plot

---
+++ tasks/G/GMT/Create-A-Xy-Plot/task.jl
@@ -0,0 +1,22 @@
+# Task: Create A Xy Plot
+# Package: GMT
+# Author: @joa-quim
+# Created: 2025-05-04
+# Sample timings: install in 218.9s, run in 7.652s
+
+__t1 = time()
+
+using GMT
+
+__t2 = time()
+
+plot(rand(5,2))
+rm("gmt.history")
+
+__t3 = time()
+
+__t_using = __t2 - __t1
+__t_script = __t3 - __t2
+__t_total = __t3 - __t1
+println(stdout, "$__t_using, $__t_script, $__t_total seconds")
+
